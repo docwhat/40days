34039206d05de4a67c94af67d3631de4e6e01c61
Christian Höltje <docwhat@gerf.org>
2013-02-28 01:20:58 -0500

    Improved the overall UX
    * Added instructions (hidden for touch devices)
    * Added support for up/down (+/-1) and page up/down (+/-10)
    * Handles corner cases better (empty input, etc.)

--- coffeescripts/app.coffee
+++ coffeescripts/app.coffee
@@ -4,22 +4,55 @@ jQuery ->
   one_day = 24 * one_hour
 
   DAYS_OF_THE_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December']
 
   handleChange = ->
     days = parseInt($(this).val(), 10)
-    if !isNaN(days)
-      today = new Date()
-      future_date = new Date(today.getTime() + one_day * days)
-      week_day = DAYS_OF_THE_WEEK[future_date.getDay()]
-      day_of_month = future_date.getDate()
-      month = MONTHS[future_date.getMonth()]
-      year = future_date.getFullYear()
-      $('#future-date').text(week_day + ", " + month + " " + day_of_month + ", " + year)
+    days = 0 if isNaN(days)
+
+    today = new Date()
+    future_date = new Date(today.getTime() + one_day * days)
+    week_day = DAYS_OF_THE_WEEK[future_date.getDay()]
+    day_of_month = future_date.getDate()
+    month = MONTHS[future_date.getMonth()]
+    year = future_date.getFullYear()
+    $('#future-date').text(week_day + ", " + month + " " + day_of_month + ", " + year)
+
+  handleKeyDown = ->
+    adjust = 0
+    if event.keyCode == 38 # Up
+      adjust = 1
+    else if event.keyCode == 40 # down
+      adjust = -1
+    else if event.keyCode == 33 # down
+      adjust = 10
+    else if event.keyCode == 34 # down
+      adjust = -10
+
+    if adjust != 0
+      new_value = parseInt($(this).val(), 10) + adjust
+      if isNaN(new_value)
+        new_value = adjust
+      if new_value >= 0 && new_value <= 999
+        $(this).val(new_value)
+        event.preventDefault()
+        $(this).change()
 
   handleKeyPress = ->
     char = String.fromCharCode event.keyCode
-    if !char.match /^[0-9]?$/
+    if !char.match(/^[0-9]?$/)
       event.preventDefault()
       $(this).change()
 
@@ -29,6 +62,7 @@ jQuery ->
   $("#number-of-days")
     .focus()
     .change(handleChange)
+    .keydown(handleKeyDown)
     .keypress(handleKeyPress)
     .keyup(handleChange)
     .change()
--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,7 @@
 (function() {
 
   jQuery(function() {
-    var DAYS_OF_THE_WEEK, MONTHS, handleChange, handleKeyPress, hourlyUpdate, one_day, one_hour;
+    var DAYS_OF_THE_WEEK, MONTHS, handleChange, handleKeyDown, handleKeyPress, hourlyUpdate, one_day, one_hour;
     one_hour = 60 * 60 * 1000;
     one_day = 24 * one_hour;
     DAYS_OF_THE_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -9,14 +9,39 @@
     handleChange = function() {
       var day_of_month, days, future_date, month, today, week_day, year;
       days = parseInt($(this).val(), 10);
-      if (!isNaN(days)) {
-        today = new Date();
-        future_date = new Date(today.getTime() + one_day * days);
-        week_day = DAYS_OF_THE_WEEK[future_date.getDay()];
-        day_of_month = future_date.getDate();
-        month = MONTHS[future_date.getMonth()];
-        year = future_date.getFullYear();
-        return $('#future-date').text(week_day + ", " + month + " " + day_of_month + ", " + year);
+      if (isNaN(days)) {
+        days = 0;
+      }
+      today = new Date();
+      future_date = new Date(today.getTime() + one_day * days);
+      week_day = DAYS_OF_THE_WEEK[future_date.getDay()];
+      day_of_month = future_date.getDate();
+      month = MONTHS[future_date.getMonth()];
+      year = future_date.getFullYear();
+      return $('#future-date').text(week_day + ", " + month + " " + day_of_month + ", " + year);
+    };
+    handleKeyDown = function() {
+      var adjust, new_value;
+      adjust = 0;
+      if (event.keyCode === 38) {
+        adjust = 1;
+      } else if (event.keyCode === 40) {
+        adjust = -1;
+      } else if (event.keyCode === 33) {
+        adjust = 10;
+      } else if (event.keyCode === 34) {
+        adjust = -10;
+      }
+      if (adjust !== 0) {
+        new_value = parseInt($(this).val(), 10) + adjust;
+        if (isNaN(new_value)) {
+          new_value = adjust;
+        }
+        if (new_value >= 0 && new_value <= 999) {
+          $(this).val(new_value);
+          event.preventDefault();
+          return $(this).change();
+        }
       }
     };
     handleKeyPress = function() {
@@ -30,7 +55,7 @@
     hourlyUpdate = function() {
       return $("#number-of-days").change();
     };
-    $("#number-of-days").focus().change(handleChange).keypress(handleKeyPress).keyup(handleChange).change();
+    $("#number-of-days").focus().change(handleChange).keydown(handleKeyDown).keypress(handleKeyPress).keyup(handleChange).change();
     return window.setInterval(hourlyUpdate, one_hour);
   });
 
